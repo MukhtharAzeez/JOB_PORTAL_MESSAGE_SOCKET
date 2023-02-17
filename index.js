94d3b248f413c6e31517c3144c33bc69dc29c2a5
Mukhthar Azeez <mukhtharazeez28@gmail.com>
2023-02-17 10:42:37 +0530

    Message and Notification socket

---
+++ index.js
@@ -0,0 +1,22 @@
+const io = require("socket.io")(8400, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+
+io.on("connection", (socket) => {
+  io.emit('me', socket.id);
+  socket.on('callUser', ({ userToCall, signalData, from, name }) => {
+    io
+      .to(userToCall)
+      .emit('callUser', { signal: signalData, from, name });
+  });
+  socket.on('answerCall', (data) => {
+    io.to(data.to).emit('callAccepted', data.signal);
+  });
+
+  socket.on("disconnect", () => {
+    io.emit('Call ended');
+  });
+});
